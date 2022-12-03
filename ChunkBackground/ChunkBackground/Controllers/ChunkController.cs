ce325bdf3e35830c95347019e626a71774237930
Гудзь Эдуард <eduard.gudz@am-intech.ru>
2022-12-03 14:05:43 +0300

    init_projects

---
+++ ChunkBackground/ChunkBackground/Controllers/ChunkController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata;
+
+namespace ChunkBackground.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChunkController : ControllerBase
+    {
+        [HttpPost]
+        [Route("UploadFile")]
+        public async Task<bool> createFile(IFormFile file, [FromForm] string numberChunk)
+        {
+            try
+             {
+                if (!Directory.Exists("temp"))
+                {
+                    Directory.CreateDirectory("temp");
+                }
+                 using (var fileStream = new FileStream($"temp/temp{numberChunk}", FileMode.Create))
+                 {
+                     await file.CopyToAsync(fileStream);
+                 }
+                 return true;
+             }
+             catch
+             {
+                 return false;
+             }
+        }
+    }
+}
